2fe45e6610f3a39e19ba82599784395a8537d0b4
Oliwier Kulczycki <oleknuso282@gmail.com>
2024-08-05 14:06:42 +0200

    Working pixel

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 Attempt at snake game with the sdl2 lib in c++.
 
 ## Help with SDL2
-compiling and importing: https://stackoverflow.com/questions/42306300/compile-a-c-sdl2-program-using-the-gcc-compiler-on-a-mac
+### Setup - Importing and Compilation
+https://stackoverflow.com/questions/42306300/compile-a-c-sdl2-program-using-the-gcc-compiler-on-a-mac - basic
+
 https://www.youtube.com/watch?v=jUZZC9UXyFs - vid mightve saved my life
+
 https://stackoverflow.com/questions/77355246/how-do-i-override-the-malicious-software-check-for-the-sdl2-framework-on-mac - security warning bypass
+
+### Using the event queue to catch inputs
+https://lazyfoo.net/tutorials/SDL/03_event_driven_programming/index.php
 
 
--- main.cpp
+++ main.cpp
@@ -4,13 +4,39 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    SDL_RenderDrawPoint(renderer, 640/2, 480/2);
+    SDL_RenderPresent(renderer);
+    
+    // Apparently Need this event handler on macos!
+    // https://stackoverflow.com/questions/65159971/how-to-create-a-window-with-sdl2-on-macos
+    bool quit = false;
+            // Event handler
+            SDL_Event e;
+            // While application is running
+            while( !quit )
+            {
+                // Handle events on queue
+                while( SDL_PollEvent( &e ) != 0 ) // poll for event
+                {
+                    // User requests quit
+                    if( e.type == SDL_QUIT ) // unless player manually quits
+                    {
+                        quit = true;
+                    }
+                }
+            }
 
 
-    cout << "testtest123" << endl;
-    SDL_Quit();
+
     return 0;
 }
